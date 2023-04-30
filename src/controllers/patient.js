d80a1d9137c7d5590704ed83b702a3bcc1b23123
Hast11 <andres2000gil@gmail.com>
2023-04-30 19:42:16 +0200

    Muchos metodos!!

--- src/controllers/patient.js
+++ src/controllers/patient.js
@@ -1,23 +1,51 @@
 const {models} = require('../models');
+const patient = require('../models/patient');
 
 // Muestra la informacion de un paciente
 exports.read = async function (patientId) {
-    // Rellene aqui ...
+   let paciente = await models.Patient.findByPk(patientId);
+
+   return paciente;
 }
 
 // Crea un paciente en un hospital
 exports.create = async function (hospitalId, name, surname, dni) {
-    // Rellene aqui ...
+    try{
+        let paciente = models.Patient.build({
+            name:name, surname:surname, dni:dni, hospitalId:hospitalId
+        });//Creamos paciente con los datos que nos dan
+        patient = await patient.save({fields: ["name","surname", "dni", "hospitalId"]}); //Guardar en la base de datos
+        console.log('Paciente creado correctamente');//Checkin de todo en orden
+        return paciente;//Devolvemos lo creado
+    } catch(error){
+        console.log(error);
+    }  
 }
 
 // Actualiza un paciente
 exports.update = async function (patientId, name, surname, dni) {
-    // Rellene aqui ...
+    let paciente = await models.Patient.findByPk(patientId);//Buscamos paciente
+    paciente.name= name; //Cambiamos datos
+    paciente.surname = surname;
+    paciente.dni = dni;
+    try{
+        await paciente.save({fields: ["name", "surname", "dni"]});
+        console.log('Paciente actualizado!')
+    }catch(error){
+        console.log(error);
+    }
 }
 
 // Borra un paciente
 exports.delete = async function (patientId) {
-    // Rellene aqui ...
+    try{
+        await models.Patient.destroy({where:{
+            id: patientId
+        }});
+        console.log('Paciente destruido!')
+    }catch(error){
+        console.log(error);
+    }
 }
 
 
